f5d20e4bfa43ad68a1b0217f09c43d3c6e626494
seviaybulut <34681520+seviaybulut@users.noreply.github.com>
2017-12-20 09:13:24 +0300

    Add files via upload

---
+++ binary kodu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1, kalan;
+            string yazikalan = "";
+            Console.WriteLine("99<iki sayı girin<999");
+            Console.Write("sayıyı giriniz..:");
+            sayi1 = int.Parse(Console.ReadLine());
+            while (sayi1 != 0)
+            {
+                kalan = sayi1 % 2;
+                sayi1 = sayi1 / 2;
+
+                yazikalan =
+                yazikalan = kalan.ToString() + yazikalan;
+                //console.writeLine("2'lik Sistem:{0},kalan);
+            }
+            Console.WriteLine("2'lik Sistemdeki Karşılığı : {0}", yazikalan);
+            Console.ReadKey();
+
+        }
+    }
+}
