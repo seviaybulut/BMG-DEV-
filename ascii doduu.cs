8c3f42777d0fa63a84460a5e0ab8078aa350017b
seviaybulut <34681520+seviaybulut@users.noreply.github.com>
2017-12-20 09:13:06 +0300

    Add files via upload

---
+++ ascii doduu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad;
+            Console.Write("adınızı girin:");
+            ad=Console.ReadLine();
+            foreach(char a in ad)
+                Console.Write((char) (a+10));
+            Console.ReadKey();
+
+        }
+    }
+}
