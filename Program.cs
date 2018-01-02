942eac3a738e9fb5bc33fdd355d795cf028bb79a
seviaybulut <34681520+seviaybulut@users.noreply.github.com>
2018-01-02 23:04:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+          // Oklid(a,b)
+//Özyinelemeli Öklid Algoritması kullanarak OBEB bulma algoritması
+
+ PozıtıfTamSayi Oklid(PozıtıfTamSayi a,PozıtıfTamSayi b)
+{
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+
+    if (a > b)
+        return Oklid(a % b, b);
+    else
+        return Oklid(a, b % a);
+
+}
+       }
+     }
+   }
